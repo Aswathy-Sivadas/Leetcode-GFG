76bbbcf096434baf69965de08adb460901e3f8fd
Aswathy S <167768744+Aswathy-Sivadas@users.noreply.github.com>
2026-06-16 18:57:32 +0530

    Time: 4 ms (76.47%) | Memory: 55 MB (75.00%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuffer sb=new StringBuffer();
+        for(int i=0;i<s.length();i++)
+        {
+            if(Character.isLetter(s.charAt(i)))
+            {
+                sb.append(s.charAt(i));
+            }
+            else if(s.charAt(i)=='#')
+            {
+                sb.append(sb);
+            }
+            else if(s.charAt(i)=='*')
+            {
+                if(sb.length()!=0)
+                {
+                    sb.deleteCharAt(sb.length()-1);
+                }
+                
+            }
+            else if(s.charAt(i)=='%')
+            {
+                sb.reverse();
+            }
+
+        }
+        return sb.toString();
+    }
+}
